2324e32324977fa552491934878f6425787da41b
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2021-06-28 13:57:52 +0200

    changes fone for dockerizarion

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,3 @@
 import { Sequelize, DataTypes } from "sequelize"
-
-export const sequelize = new Sequelize(
-	"postgres://postgres:hpscanjet2300c@localhost:5432/totMoney"
-)
+const ConnectionUrl: string = process.env.DbURL ? process.env.DbURL : ""
+export const sequelize = new Sequelize(ConnectionUrl)
